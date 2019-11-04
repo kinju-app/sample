575b492905aad0fedfd169da13c460674402f91a
Kinjal Patel <kinjal.p@applocum.org>
2019-11-04 11:59:06 +0530

    Added some test cases

--- spec/greet_spec.rb
+++ spec/greet_spec.rb
@@ -1,8 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Greet Viewer', type: :feature do
+
 	scenario 'home page' do
 		visit root_path
-		expect(page).to have_content('Hello')
+		expect(page.body).to have_content('Hello')
+	end
+
+	context "While Testing Hello" do
+		it "It should display Log in" do
+			visit root_pat
+			expect(page).to have_content('Log in')
+		end
+		it "It should display Home" do
+			visit root_path
+			expect(page).to have_content('Home')
+		end		
+
 	end
 end
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-         
+
+	def setup
+		@book = Book.new(name: "New Book")
+		@user = User.create(username: "Name", email: "email@email.com", password: "password")		
+	end
+
+	test "category should be valid" do
+		sign_in_as(@user,"password")
+		assert @book.valid?
+	end
 end
